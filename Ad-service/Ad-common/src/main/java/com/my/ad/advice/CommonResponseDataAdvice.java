4191cede4748ed72ddab724f92c8c83de2e13928
Kong <2309173181@qq.com>
2023-11-10 18:57:49 +0800

    ww123

--- Ad-service/Ad-common/src/main/java/com/my/ad/advice/CommonResponseDataAdvice.java
+++ Ad-service/Ad-common/src/main/java/com/my/ad/advice/CommonResponseDataAdvice.java
@@ -14,11 +14,11 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 @RestControllerAdvice
 public class CommonResponseDataAdvice implements ResponseBodyAdvice<Object> {
 
+    // 判断是否支持对响应体的处理
     @Override
-    @SuppressWarnings("all")
     public boolean supports(MethodParameter methodParameter,
                             Class<? extends HttpMessageConverter<?>> aClass) {
-
+        // 如果控制器类或方法上标记了IgnoreResponseAdvice注解，则不处理响应
         if (methodParameter.getDeclaringClass().isAnnotationPresent(
                 IgnoreResponseAdvice.class
         )) {
@@ -31,28 +31,26 @@ public class CommonResponseDataAdvice implements ResponseBodyAdvice<Object> {
             return false;
         }
 
-        return true;
+        return true; // 其他情况均进行处理
     }
 
+    // 在响应体写入之前进行处理
     @Nullable
     @Override
-    @SuppressWarnings("all")
     public Object beforeBodyWrite(@Nullable Object o,
                                   MethodParameter methodParameter,
                                   MediaType mediaType,
                                   Class<? extends HttpMessageConverter<?>> aClass,
                                   ServerHttpRequest serverHttpRequest,
                                   ServerHttpResponse serverHttpResponse) {
-
-        CommonResponse<Object> response = new CommonResponse<>(0, "");
+        CommonResponse<Object> response = new CommonResponse<>(0, ""); // 创建通用响应对象
         if (null == o) {
-            return response;
+            return response; // 如果响应体为空，则返回默认通用响应
         } else if (o instanceof CommonResponse) {
-            response = (CommonResponse<Object>) o;
+            response = (CommonResponse<Object>) o; // 如果响应体已经是CommonResponse类型，则直接使用
         } else {
-            response.setData(o);
+            response.setData(o); // 否则将响应体作为数据放入通用响应对象
         }
-
-        return response;
+        return response; // 返回经过处理后的通用响应对象
     }
 }
