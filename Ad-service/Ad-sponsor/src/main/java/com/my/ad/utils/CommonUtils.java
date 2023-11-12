f9b55e1842ae4faa5ccc528297a4d344e409d6f9
Kong <2309173181@qq.com>
2023-11-12 21:19:12 +0800

    service adplan

--- Ad-service/Ad-sponsor/src/main/java/com/my/ad/utils/CommonUtils.java
+++ Ad-service/Ad-sponsor/src/main/java/com/my/ad/utils/CommonUtils.java
@@ -9,24 +9,34 @@ import java.util.Date;
 
 public class CommonUtils {
 
-    private static String[] parsePatterns = {
-            "yyyy-MM-dd", "yyyy/MM/dd", "yyyy.MM.dd"
-    };
+    // 定义日期格式的数组，用于解析日期字符串
+    private static String[] parsePatterns = {"yyyy-MM-dd", "yyyy/MM/dd", "yyyy.MM.dd"};
 
+    /**
+     * 对输入的字符串进行MD5加密，并返回大写形式的加密结果
+     *
+     * @param value 要进行MD5加密的字符串
+     * @return 加密后的结果
+     */
     public static String md5(String value) {
-
         return DigestUtils.md5Hex(value).toUpperCase();
     }
 
-    public static Date parseStringDate(String dateString)
-            throws AdException {
-
+    /**
+     * 将日期字符串解析为Date对象
+     *
+     * @param dateString 待解析的日期字符串
+     * @return 解析后得到的Date对象
+     * @throws AdException 如果解析过程中发生异常，则抛出AdException
+     */
+    public static Date parseStringDate(String dateString) throws AdException {
         try {
-            return DateUtils.parseDate(
-                    dateString, parsePatterns
-            );
+            // 使用预定义的日期格式数组来尝试解析日期字符串
+            return DateUtils.parseDate(dateString, parsePatterns);
         } catch (Exception ex) {
+            // 捕获异常并抛出自定义的AdException异常
             throw new AdException(ex.getMessage());
         }
     }
+
 }
--- Ad-service/Ad-sponsor/src/main/java/com/my/ad/vo/AdPlanRequest.java
+++ Ad-service/Ad-sponsor/src/main/java/com/my/ad/vo/AdPlanRequest.java
@@ -6,19 +6,20 @@ import lombok.NoArgsConstructor;
 import org.apache.commons.lang.StringUtils;
 
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+@Data  // Lombok 注解，自动生成 getter、setter、equals、hashCode 等方法
+@NoArgsConstructor  // Lombok 注解，生成一个无参构造方法
+@AllArgsConstructor  // Lombok 注解，生成一个包含所有参数的构造方法
+
 public class AdPlanRequest {
 
-    private Long id;
-    private Long userId;
-    private String planName;
-    private String startDate;
-    private String endDate;
+    private Long id;  // 广告计划的唯一标识符
+    private Long userId;  // 用户ID
+    private String planName;  // 计划名称
+    private String startDate;  // 计划开始日期
+    private String endDate;  // 计划结束日期
 
     public boolean createValidate() {
-
+        // 用于验证创建广告计划时的参数是否有效
         return userId != null
                 && !StringUtils.isEmpty(planName)
                 && !StringUtils.isEmpty(startDate)
@@ -26,12 +27,12 @@ public class AdPlanRequest {
     }
 
     public boolean updateValidate() {
-
+        // 用于验证更新广告计划时的参数是否有效
         return id != null && userId != null;
     }
 
     public boolean deleteValidate() {
-
+        // 用于验证删除广告计划时的参数是否有效
         return id != null && userId != null;
     }
 }
