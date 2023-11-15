fbc13e99246af4f73de137cb0421253ccc204db8
Kong <2309173181@qq.com>
2023-11-15 18:46:32 +0800

    index

---
+++ Ad-service/Ad-sponsor/src/main/java/com/my/ad/controller/AdUnitOPController.java
@@ -0,0 +1,77 @@
+package com.my.ad.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.my.ad.exception.AdException;
+import com.my.ad.service.IAdUnitService;
+import com.my.ad.vo.AdUnitDistrictRequest;
+import com.my.ad.vo.AdUnitDistrictResponse;
+import com.my.ad.vo.AdUnitItRequest;
+import com.my.ad.vo.AdUnitItResponse;
+import com.my.ad.vo.AdUnitKeywordRequest;
+import com.my.ad.vo.AdUnitKeywordResponse;
+import com.my.ad.vo.AdUnitRequest;
+import com.my.ad.vo.AdUnitResponse;
+import com.my.ad.vo.CreativeUnitRequest;
+import com.my.ad.vo.CreativeUnitResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Slf4j
+@RestController
+public class AdUnitOPController {
+
+    private final IAdUnitService adUnitService;
+
+    @Autowired
+    public AdUnitOPController(IAdUnitService adUnitService) {
+        this.adUnitService = adUnitService;
+    }
+
+    @PostMapping("/create/adUnit")
+    public AdUnitResponse createUnit(
+            @RequestBody AdUnitRequest request) throws AdException {
+        log.info("ad-sponsor: createUnit -> {}",
+                JSON.toJSONString(request));
+        return adUnitService.createUnit(request);
+    }
+
+    @PostMapping("/create/unitKeyword")
+    public AdUnitKeywordResponse createUnitKeyword(
+            @RequestBody AdUnitKeywordRequest request
+            ) throws AdException {
+        log.info("ad-sponsor: createUnitKeyword -> {}",
+                JSON.toJSONString(request));
+        return adUnitService.createUnitKeyword(request);
+    }
+
+    @PostMapping("/create/unitIt")
+    public AdUnitItResponse createUnitIt(
+            @RequestBody AdUnitItRequest request
+            ) throws AdException {
+        log.info("ad-sponsor: createUnitIt -> {}",
+                JSON.toJSONString(request));
+        return adUnitService.createUnitIt(request);
+    }
+
+    @PostMapping("/create/unitDistrict")
+    public AdUnitDistrictResponse createUnitDistrict(
+            @RequestBody AdUnitDistrictRequest request
+            ) throws AdException {
+        log.info("ad-sponsor: createUnitDistrict -> {}",
+                JSON.toJSONString(request));
+        return adUnitService.createUnitDistrict(request);
+    }
+
+    @PostMapping("/create/creativeUnit")
+    public CreativeUnitResponse createCreativeUnit(
+            @RequestBody CreativeUnitRequest request
+            ) throws AdException {
+        log.info("ad-sponsor: createCreativeUnit -> {}",
+                JSON.toJSONString(request));
+        return adUnitService.createCreativeUnit(request);
+    }
+}
